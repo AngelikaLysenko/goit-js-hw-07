e62228ba6c7984290ae3303449d30e72623f39ac
AngelikaLysenko <angelika17.8875@gmail.com>
2021-06-27 15:22:36 +0300

    exercise 8

--- js/task-08.js
+++ js/task-08.js
@@ -8,3 +8,44 @@
 // лисинеры на кнопки
 // 0 - 30,30
 // 1 - 40,40
+
+const boxDiv = document.getElementById('boxes');
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-action="render"]');
+const destroyBtn = document.querySelector('[data-action="destroy"]');
+const INITIAL_ITEM_SIZE = 30;
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+let itemSize = INITIAL_ITEM_SIZE;
+
+function createBoxes() {
+    const amount = input.value;
+
+    for(let i = 0; i < amount; i++) {
+        const divItem = document.createElement('div');
+        divItem.style.backgroundColor = getRandomColor();
+        divItem.style.width = `${itemSize}px`;
+        divItem.style.height = `${itemSize}px`;
+        itemSize += 20;
+
+        boxDiv.appendChild(divItem);
+    }
+};
+
+function getRandom(min, max) {
+    return Math.ceil(Math.random()* (max - min) + min);
+};
+
+function destroyBoxes(){
+    boxDiv.innerHTML = '';
+    itemSize = INITIAL_ITEM_SIZE;
+};
+
+function getRandomColor(){
+const red = getRandom(0,255);
+const green = getRandom(0,255);
+const blue = getRandom(0,255);
+return `rgb(${red}, ${green}, ${blue})`;
+};
